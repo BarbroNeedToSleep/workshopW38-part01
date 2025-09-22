4ce2bab9f8bd27f9169c41c12c60233abd702ae0
BarbroNeedToSleep <55791445+BarbroNeedToSleep@users.noreply.github.com>
2025-09-22 18:53:49 +0200

    Sett upp for project library spring boot whit lombok.

---
+++ src/main/java/se/lexicon/workshopweek38part01/WorkshopWeek38Part01Application.java
@@ -0,0 +1,14 @@
+package se.lexicon.workshopweek38part01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopWeek38Part01Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WorkshopWeek38Part01Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/workshopweek38part01/WorkshopWeek38Part01ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshopweek38part01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopWeek38Part01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
