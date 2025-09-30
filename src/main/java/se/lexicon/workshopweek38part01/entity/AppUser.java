2f4abafecd5fd23445c92bb3b231422775d43af3
BarbroNeedToSleep <55791445+BarbroNeedToSleep@users.noreply.github.com>
2025-09-30 16:46:28 +0200

    Added bidriktinal join from appuser to bookloan. added needed methods to the Appuser. had already addded the needed methods to book and author

--- src/main/java/se/lexicon/workshopweek38part01/entity/AppUser.java
+++ src/main/java/se/lexicon/workshopweek38part01/entity/AppUser.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 
@@ -34,6 +36,9 @@ public class AppUser {
     @JoinColumn(name = "details_id", nullable = false, unique = true)
     private Details userDetails;
 
+    @OneToMany(mappedBy = "borrower", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BookLoan> bookLoans = new ArrayList<>();
+
     @PrePersist
     public void onCreate() {
         if (regDate == null) {
@@ -45,6 +50,16 @@ public class AppUser {
         this.username = username;
         this.password = password;
         this.userDetails = userDetails;
+    }
+
+    public void addBookLoan(BookLoan loan) {
+        bookLoans.add(loan);
+        loan.setBorrower(this);
+    }
+
+    public void removeBookLoan(BookLoan loan) {
+        bookLoans.remove(loan);
+        loan.setBorrower(null);
     }
 
 
